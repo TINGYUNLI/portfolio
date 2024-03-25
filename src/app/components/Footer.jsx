13e4245e652c8e7383e14985010c0a5e4e90afaf
TINGYUNLI <rita891013@gmail.com>
2024-03-25 18:22:11 +0800

    add footer and main page layout

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border border-t-[#33353F] border-r-transparent border-l-transparent border-b-transparent text-textColor">
+      <div className="container p-8 flex justify-between">
+        <span>MyPortfolio .</span>
+        <p className="text-slate-600">All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
